7a7b201d9157f6e2cd01e69b1868e10d7254acf8
Shann Ezmat <shannezmat@gmail.com>
2019-07-18 17:51:57 -0400

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-  state = {
-    foobar: '123'
-  }
-
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidMount = () => {
-    console.log("didMount");
-    console.log(this.state);
-  }
-
-  onButtonClicked = () => {
-    console.log('foobar');
-  }
-
-  render = () => {
+class App extends Component {
+  render() {
     return (
-      <div className="App">
-        <button onClick={ this.onButtonClicked }>Click Me!</button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <h1>Hello World</h1>
     );
   }
 }
