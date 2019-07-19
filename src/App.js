9626b06fb5d1ce95e17c9185f2c07dcb21e3c103
Shann Ezmat <shannezmat@gmail.com>
2019-07-19 15:35:31 -0400

    homepage is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import pokeball from '../pokeball.png';
+//import pokeball from 'pokeball.png';
 
 class App extends Component {
   render() {
     return (
-      <h1>Is this working?</h1>;
-      <img src={Pokeball} alt="A Pokeball"/>;
-    ;
+      <h1>Is this working?</h1>
+    )
   }
 }
-
 export default App;
