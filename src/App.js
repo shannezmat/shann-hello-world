4d073a9e4ca8b0cf043672fdb22dfcb8e4c01171
Shann Ezmat <shannezmat@gmail.com>
2019-07-19 19:07:53 -0400

    Fetched API for gengar and displayed his name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-//import pokeball from 'pokeball.png';
+import pokeball from './pokeball.png';
 
 class App extends Component {
+
+  state = {
+    loading: true,
+    loaded: null,
+  }
+  
+    async componentDidMount(){
+      const url="https://pokeapi.co/api/v2/pokemon/gengar/";
+      const response = await fetch(url);
+      const data = await response.json();
+      this.setState({loaded: data,loading:false});
+      console.log(data);
+  }
   render() {
     return (
-      <h1>Is this working?</h1>
-    )
+      <div>
+        {this.state.loading || !this.state.loaded ? 
+        ( <div>loading...</div> ) : 
+        ( <div>
+          <h1 id="pokename">{this.state.loaded.name}</h1>
+        </div> 
+        )}
+      </div>
+    ); 
   }
 }
 export default App;
+
+
